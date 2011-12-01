abbf49c5b4ca72c6ee6526166ae4f753fd952734
Mark Overmeer <mark@overmeer.net>
2011-12-01 13:37:43 +0000

    Import of MARKOV/Geo-Format-Envisat-0.03 from CPAN.
    gitpan-cpan-distribution: Geo-Format-Envisat
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         MARKOV/Geo-Format-Envisat-0.03.tar.gz
    gitpan-cpan-author:       MARKOV
    gitpan-cpan-maturity:     released

--- lib/Geo/Format/Envisat.pm
+++ lib/Geo/Format/Envisat.pm
@@ -1,13 +1,13 @@
-# Copyrights 2008-2009 by Mark Overmeer.
+# Copyrights 2008-2011 by Mark Overmeer.
 #  For other contributors see ChangeLog.
 # See the manual pages for details on the licensing terms.
-# Pod stripped from pm file by OODoc 1.05.
+# Pod stripped from pm file by OODoc 2.00.
 use warnings;
 use strict;
 
 package Geo::Format::Envisat;
 use vars '$VERSION';
-$VERSION = '0.02';
+$VERSION = '0.03';
 
 use base 'Exporter';
 
